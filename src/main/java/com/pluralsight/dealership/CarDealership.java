b80db5d91210c7736b308a86418bb72e589ad4df
keziazahiljohn <keziazahiljohn@gmail.com>
2026-05-15 11:43:10 -0400

    contract abstract class

---
+++ src/main/java/com/pluralsight/dealership/CarDealership.java
@@ -0,0 +1,8 @@
+package com.pluralsight.dealership;
+
+public class CarDealership {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+}
---
+++ src/main/java/com/pluralsight/dealership/Contract.java
@@ -0,0 +1,49 @@
+package com.pluralsight.dealership;
+
+public abstract class Contract {
+    private String dateOfContract;
+    private String name;
+    private String email;
+    private Vehicle vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String dateOfContract, String name, String email, Vehicle vehicleSold) {
+        this.dateOfContract = dateOfContract;
+        this.name = name;
+        this.email = email;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public String getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(String dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+}
